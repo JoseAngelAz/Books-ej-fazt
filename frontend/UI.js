fa0568b5b22da01b5154997bbbd156fa3950e6ca
José Angel Azucena <angelvanitas555@gmail.com>
2020-04-25 05:16:04 -0600

    cambios en stilos de la vista

--- frontend/UI.js
+++ frontend/UI.js
@@ -13,13 +13,13 @@ class UI {
       div.className = "";
       div.innerHTML = `
                 
-                <div class= "card m-2 border border-dark rounded">
+                <div class= "card m-2 border shadow-lg  rounded">
                 <div class= "row">                
                     <div class= "col-md-4">
-                    <img class="img-fuid card-image-top cover" src="http://localhost:3600${book.imagePath}" alt="image">
+                    <img class="img-fuid card-image-top cover shadow-lg" src="http://localhost:3600${book.imagePath}" alt="image">
                     </div>
-                    <div class= "col-md-8">
-                    <div class= "card-block px-2 border border-white rounded">
+                    <div class= "col-md-7 fondo-carta text-center">
+                    <div class= "carta-bloque card-block px-2 rounded col-12">
                         <h4 class="card-title" >${book.title}</4>
                         <p class="card-text">${book.author}</p>
                         <p class="card-text">${book.isbn}</p>
@@ -27,11 +27,11 @@ class UI {
                         <p class="card-text">${book.status_life}</p>
                         <p class="card-text">${book.status_book}</p>
 
-                        <a href="#" class="btn btn-danger delete" _id="${book._id}">X</a>
+                        <a href="#" class="btn btn-x btn-block rounded delete" _id="${book._id}">ELIMINAR</a>
                     </div>    
                     </div>
                 </div>
-                <div class="card footer border border-white rounded ">
+                <div class="card footer border border-white rounded ml-3 ">
                 ${format(book.crated_at)}
                 </div>
                 </div>
